153417c1f419ffc9676a0313d0897707e5be9ba8
Kevin Fanning <kfanning5@hotmail.com>
2017-11-14 19:24:00 -0600

    add json

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+
+  def product_method
+    product = Product.all
+    render json: product.as_json
+  end
+
+  def product_first_method
+    product = Product.first
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/product_url" => "products#product_method"
+  get "/product_first_url" => "products#product_first_method"
 end
