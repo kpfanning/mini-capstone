49cd91cb86919fd7fde13389a68701e1df19984f
Kevin Fanning <kfanning5@hotmail.com>
2017-11-14 19:08:06 -0600

    add products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Dog Toy", price: "2", image:"", description: "Soft Plush")
+Product.create(name: "Bone", price: "1", image:"", description: "Crunchy Beef")
+Product.create(name: "Collar", price: "5", image:"", description: "Round")
+
+
